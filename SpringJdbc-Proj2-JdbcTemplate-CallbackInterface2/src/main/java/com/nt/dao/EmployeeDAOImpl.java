7e00daffe28512309f24cad01a31ae1cbc8ca626
iam-biswajeet <biswajitsahoo9955@gmail.com>
2022-11-04 20:11:31 +0530

    spring jdbc rowmapper

--- SpringJdbc-Proj2-JdbcTemplate-CallbackInterface2/src/main/java/com/nt/dao/EmployeeDAOImpl.java
+++ SpringJdbc-Proj2-JdbcTemplate-CallbackInterface2/src/main/java/com/nt/dao/EmployeeDAOImpl.java
@@ -1,35 +1,49 @@
 package com.nt.dao;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.nt.model.Employee;
+
 @Repository("empDAO")
-public class EmployeeDAOImpl implements IEmployeeDAO{
-	private static final String GET_EMP_BY_EMP_NO="SELECT EMPNO,ENAME,JOB,SAL FROM EMP WHERE EMPNO=? ";
+public class EmployeeDAOImpl implements IEmployeeDAO {
+	private static final String GET_EMP_BY_EMP_NO = "SELECT EMPNO,ENAME,JOB,SAL FROM EMP WHERE EMPNO=? ";
 	@Autowired
 	JdbcTemplate jt;
 	@Override
 	public Employee getEmployeeByNo(int no) {
-		Employee emp=jt.queryForObject(GET_EMP_BY_EMP_NO, new EmployeeRowMapper(),no);
+		Employee emp=jt.queryForObject(GET_EMP_BY_EMP_NO, new BeanPropertyRowMapper<Employee>(Employee.class),no);
 		return emp;
 	}
-	static class EmployeeRowMapper implements RowMapper<Employee>{
-		@Override
-		public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
-			Employee emp=new Employee();
-			emp.setEmpno(rs.getInt(1));
-			emp.setEname(rs.getString(2));
-			emp.setJob(rs.getString(3));
-			emp.setSal(rs.getFloat(4));
-			return emp;
-		}
-		
-	}
+	/*
+	 * @Override public Employee getEmployeeByNo(int no) { Employee
+	 * emp=jt.queryForObject(GET_EMP_BY_EMP_NO,(ResultSet rs,int rn)->{ Employee
+	 * e=new Employee(); e.setEmpno(rs.getInt(1)); e.setEname(rs.getString(2));
+	 * e.setJob(rs.getString(3)); e.setSal(rs.getFloat(4)); return e; },no); return
+	 * emp; }
+	 */
 
+	/*
+	 * @Override public Employee getEmployeeByNo(int no) { Employee emp =
+	 * jt.queryForObject(GET_EMP_BY_EMP_NO, new RowMapper<Employee>() {
+	 * 
+	 * @Override public Employee mapRow(ResultSet rs, int rowNum) throws
+	 * SQLException { Employee e = new Employee(); e.setEmpno(rs.getInt(1));
+	 * e.setEname(rs.getString(2)); e.setJob(rs.getString(3));
+	 * e.setSal(rs.getFloat(4)); return e;
+	 * 
+	 * } }, no); return emp; }
+	 */
+	/*
+	 * @Override public Employee getEmployeeByNo(int no) { Employee
+	 * emp=jt.queryForObject(GET_EMP_BY_EMP_NO, new EmployeeRowMapper(),no); return
+	 * emp; } static class EmployeeRowMapper implements RowMapper<Employee>{
+	 * 
+	 * @Override public Employee mapRow(ResultSet rs, int rowNum) throws
+	 * SQLException { Employee emp=new Employee(); emp.setEmpno(rs.getInt(1));
+	 * emp.setEname(rs.getString(2)); emp.setJob(rs.getString(3));
+	 * emp.setSal(rs.getFloat(4)); return emp; }
+	 */
 }
